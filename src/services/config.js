b023ece588a87b52b795cb3722a717728d24b92c
Clevergy account <clevergy.app@outlook.com>
2016-02-04 13:06:47 +0100

    windows setup

--- src/services/config.js
+++ src/services/config.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.load();
 
 export const ALLOWED_SOURCE = "reading";
-export const MONGODB_URL = process.env.MONGODB_URL;
+export const MONGODB_URL = process.env.MONGODB_URL || "mongodb://localhost:27017/test";
 export const READINGS_REAL_TIME_AGGREGATES = "readings-real-time-aggregates";
 export const SITES = "sites";
 export const SENSORS = "sensors";
